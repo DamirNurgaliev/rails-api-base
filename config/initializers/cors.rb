25476a799df9fdf17c986ee43207cabdfe8e88cd
damirnurgaliev <damir.nurgaliev@flatstack.com>
2018-11-06 14:00:10 +0300

    Return  for no ENV variable cors host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(*ENV.fetch("ALLOW_REQUESTS_FROM", app_config.host).split(","))
+    origins(*ENV.fetch("ALLOW_REQUESTS_FROM", "").split(","))
 
     resource "*",
       headers: :any,
