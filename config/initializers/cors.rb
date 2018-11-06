ee79b26f4a968d39e95901de27d42d9326ec32e2
damirnurgaliev <damir.nurgaliev@flatstack.com>
2018-11-06 13:48:00 +0300

    Fix rubocop offences

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(*ENV.fetch("ALLOW_REQUESTS_FROM", app_config.host).split(","))
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: %i(get post put delete patch options head)
+      methods: %i[get post put delete patch options head]
   end
 end
