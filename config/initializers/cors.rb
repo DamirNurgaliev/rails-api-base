e484aa55097ba1c56e77761696c26999b8952107
Damir Nurgaliev <damir.nurgaliev@flatstack.com>
2019-05-30 12:01:05 +0200

    Update gems

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
 
-    resource "*",
-      headers: :any,
-      methods: %i[get post put delete patch options head]
+    resource "*", headers: :any, methods: %i[get post put delete patch options head]
   end
 end
